e6b169a647e276b9bc95f652f038a49b8bb97676
Jose Alberto Guevara Morales <59805803+CapiFrank@users.noreply.github.com>
2022-10-30 23:40:26 +0000

    Se modifica la vista de las tareas en TaskCategory

--- src/pages/TaskCategory.vue
+++ src/pages/TaskCategory.vue
@@ -1,15 +1,22 @@
 <template>
   <router-view></router-view>
   <div>
-    <table>
-      <tbody>
-        <tr v-for="todo in todos" :key="todo.id">
-          <td>{{ todo.id }}</td>
-          <td>{{ todo.title }}</td>
-          <td>{{ todo.contents }}</td>
-        </tr>
-      </tbody>
-    </table>
+ <div class="col accordion" id="accordionExample">
+  <div class="accordion-item" v-for="(todo,index) in todos" :key="todo.id" style="width:55vw; overflow:hidden;">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" v-b-toggle="'accordion-' + index" aria-expanded="true" aria-controls="todo.id" @click="selection(todo)">
+        <div>
+          {{ todo.title }}
+        </div>
+      </button>
+    </h2>
+    <div :id="'accordion-' + index" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        {{ todo.contents }}
+      </div>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
